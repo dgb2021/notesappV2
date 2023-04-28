33432491ab4b0fdacef9751afd3103c7a9b87758
Doug Barr <douglas.barr01@gmail.com>
2023-04-28 15:51:13 +0100

    Adding validation to the note model

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,5 @@
 class Note < ApplicationRecord
-
+  validates :title, presence: true, length: {minimum: 3, maximum: 100}
+  validates :body, presence: true, length: {minimum: 3, maximum: 1000}
+  validates :user_id, presence: true
 end
